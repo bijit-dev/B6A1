0912655c6dbac16f65fe9d43e7d569d59c1b938a
BIJIT DEB <bijitdeb0703@gmail.com>
2025-11-18 11:22:27 +0600

    problem 3 sovled

--- solution.ts
+++ solution.ts
@@ -2,11 +2,9 @@ function formatValue(value: string | number | boolean): string | number | boolea
   if (typeof value === "string") {
     return value.toUpperCase();
   }
-
   else if (typeof value === "number") {
     return value * 10;
   }
-
   else {
     return !value;
   }
@@ -21,5 +19,19 @@ function getLength(value: string | any[]): number {
     } 
     else {
         return 0;
+    }
+}
+
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getDetails(): string {
+        return `'Name: ${this.name}, Age: ${this.age}'`;
     }
 }
